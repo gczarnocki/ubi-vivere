77d39cfd94f6037a8504e674283f19da12f93252
Wojciech Urbanski <urban@ds.pg.gda.pl>
2016-09-24 19:28:40 +0200

    Added radiusInput component

--- HackathonFront/src/app/components/radiusInput.js
+++ HackathonFront/src/app/components/radiusInput.js
@@ -1,4 +1,15 @@
 angular.module('app.components')
   .component('radiusInput', {
-    templateUrl: 'app/components/radiusInput.html'
+    templateUrl: 'app/components/radiusInput.html',
+    controller: RadiusInputCtrl,
+    bindings: {
+      value: '<'
+    }
   })
+
+function RadiusInputCtrl () {
+  var vm = this;
+  vm.value = 1;
+
+  return vm
+}
