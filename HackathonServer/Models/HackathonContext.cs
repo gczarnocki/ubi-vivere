af2232121605caca0367c7158e414aa90b3f2811
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2016-09-24 23:12:27 +0200

    Fix pluralization in db

--- HackathonServer/Models/HackathonContext.cs
+++ HackathonServer/Models/HackathonContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 using HackathonServer.Dtos;
@@ -13,6 +14,11 @@ namespace HackathonServer.Models
         {
             Database.SetInitializer<HackathonContext>(new DropCreateDatabaseAlways<HackathonContext>());
             InitializeDatabase();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
         public HackathonContext(string connectionString) : base(connectionString)
