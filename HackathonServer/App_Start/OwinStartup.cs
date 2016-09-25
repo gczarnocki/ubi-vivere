c2716c983bd0a2b0555af8f159063f41607a006e
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2016-09-25 14:03:52 +0200

    Add Owin - static files hosting, @ wwwroot/front/

---
+++ HackathonServer/App_Start/OwinStartup.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Web.Http;
+using HackathonServer;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinStartup))]
+namespace HackathonServer
+{
+    public class OwinStartup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+
+            string exeFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string webFolder = Path.Combine(exeFolder, "/wwwroot/front");
+            appBuilder.UseStaticFiles(webFolder);
+        }
+    }
+}
--- HackathonServer/Global.asax.cs
+++ HackathonServer/Global.asax.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.Owin.Hosting;
 
 namespace HackathonServer
 {
