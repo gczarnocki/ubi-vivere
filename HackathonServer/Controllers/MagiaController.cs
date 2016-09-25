be1cf7010f4e0687d0ece08e5e0fda67843ef162
bartekx <bartek.z0@wp.pl>
2016-09-25 12:14:48 +0200

    magia api

---
+++ HackathonServer/Controllers/MagiaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HackathonServer.Dtos;
+using HackathonServer.Models;
+
+namespace HackathonServer.Controllers
+{
+    public class MagiaController : ApiController
+    {
+        // GET api/Magia
+        public PointDto[] Get()
+        {
+            using (var context = new HackathonContext())
+            {
+                return context.BusStops.Select(b => new PointDto() {X = b.X, Y = b.Y}).ToArray();
+            }
+        }
+    }
+}
---
+++ HackathonServer/Dtos/PointDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonServer.Dtos
+{
+    [DataContract]
+    public class PointDto
+    {
+        [DataMember(Name = "y")]
+        public double Y { get; set; }
+        [DataMember(Name = "x")]
+        public double X { get; set; }
+    }
+}
